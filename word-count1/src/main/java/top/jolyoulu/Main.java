935b0812fdad4eefd589ea8e35705bf05199c6c6
JolyouLu <1307633126@qq.com>
2022-09-10 17:05:13 +0800

    init

---
+++ word-count1/src/main/java/top/jolyoulu/Main.java
@@ -0,0 +1,12 @@
+package top.jolyoulu;
+
+/**
+ * @Author: JolyouLu
+ * @Date: ${DATE} ${TIME}
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
