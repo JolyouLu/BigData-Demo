9ffea37f1157c1ce36c51187989b0f36a21f11d4
皮蛋 <1139219074@qq.com>
2022-11-13 20:08:50 +0800

    Combiner合并

---
+++ simple-case/src/main/java/top/jolyoulu/wordcountcombiner/WordCountDriver.java
@@ -0,0 +1,53 @@
+package top.jolyoulu.wordcountcombiner;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import top.jolyoulu.sort.FlowCountDriver;
+
+import java.io.File;
+import java.net.URL;
+
+/**
+ * @Author: JolyouLu
+ * @Date: 2022/9/10 17:32
+ * @Version 1.0
+ */
+public class WordCountDriver {
+    public static void main(String[] args) throws Exception {
+        //写死路径测试，读取于输出到在target目录下
+        URL resource = FlowCountDriver.class.getClassLoader().getResource("test/wordcountcombiner/test_data.txt");
+        File file = new File(resource.toURI());
+        args = new String[]{
+                file.getPath(),
+                file.getParent()+"\\out"
+        };
+
+        Configuration conf = new Configuration();
+        //获取job对象
+        Job job = Job.getInstance(conf);
+        //设置jar存储位置
+        job.setJarByClass(WordCountDriver.class);
+        //关联Map和Reduce类
+        job.setMapperClass(WordCountMapper.class);
+        job.setReducerClass(WordCountReducer.class);
+        //设置Mapper阶段输出数据的key和value类型
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        //设置最终数据输出的key和value类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        //关联Combiner
+        job.setCombinerClass(WordCountCombiner.class);
+        //设置输入路径和输出路径
+        FileInputFormat.setInputPaths(job,new Path(args[0]));
+        FileOutputFormat.setOutputPath(job,new Path(args[1]));
+        //提交job
+        boolean result = job.waitForCompletion(true);
+        System.exit(result ? 0 : 1);
+    }
+}
---
+++ simple-case/src/main/java/top/jolyoulu/wordcountcombiner/WordCountMapper.java
@@ -0,0 +1,38 @@
+package top.jolyoulu.wordcountcombiner;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * @Author: JolyouLu
+ * @Date: 2022/9/10 17:09
+ * @Version 1.0
+ */
+//map阶段
+//KEYIN 输入数据的key，内容是文本所以key是偏移量
+//VALUEIN 数据数据类型value
+//KEYOUT 输出数据类型
+//VALUEOUT 数据数据value类型
+public class WordCountMapper extends Mapper<LongWritable, Text,Text, IntWritable> {
+    Text k = new Text();
+    IntWritable v = new IntWritable(1);
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        //1.获取一行
+        String line = value.toString();
+        System.out.println("key => "+key+"; value =>"+line);
+        //2.根据空格拆分
+        String[] words = line.split(" ");
+        //3.循环写出
+        for (String word : words) {
+            k.set(word);
+            context.write(k,v);
+        }
+    }
+
+}
