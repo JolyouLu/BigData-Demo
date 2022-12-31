e21a6c8ca72ad06ad7077df40178d8bcf17973c0
Jolyoulu <1139219074@qq.com>
2022-12-31 15:55:37 +0800

    修改模块名称

--- hadoop-2.x/src/main/java/top/jolyoulu/wordcount/WordCountDriver.java
+++ hadoop-2.x/src/main/java/top/jolyoulu/wordcount/WordCountDriver.java
@@ -4,6 +4,8 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.BZip2Codec;
+import org.apache.hadoop.io.compress.CompressionCodec;
 import org.apache.hadoop.mapred.lib.CombineFileInputFormat;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
